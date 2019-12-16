d834be23a2bcd5062468d90bc1e0b817db70ad53
sj.hyeon <uiandwe@gmail.com>
2019-12-16 23:17:22 +0900

    클래스 / slot. 추가

--- main.py
+++ main.py
@@ -1,68 +1,80 @@
 # -*- coding: utf-8 -*-
-# TODO 클래스화
-# TODO slot
+# TODO logger 추가 하기
+# TODO socket 클래스로 분리하기
+# TODO string to byte 함수 추가
+# TODO wsgi 호환
+# 
+
 
 import sys
 import socket
 import selectors
 import types
 
-sel = selectors.DefaultSelector()
+
+class WebFrameWork:
+    __slots__ = ["host", "port", "lsock", "sel"]
+
+    def __init__(self):
+        self.host = "127.0.0.1"
+        self.port = 65432
+        self.lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sel = selectors.DefaultSelector()
+
+    def __call__(self, *args, **kwargs):
+        self.lsock.bind((self.host, self.port))
+        self.lsock.listen()
+        print("listion on ", (self.host, self.port))
+        self.lsock.setblocking(False)
+        self.sel.register(self.lsock, selectors.EVENT_READ, data=None)
 
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept()
-    print("accepted connection from ", addr)
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
+        while True:
+            events = self.sel.select(timeout=10)
+            for key, mask in events:
+                if key.data is None:
+                    self.accept_wrapper(key.fileobj)
+                else:
+                    self.service_connection(key, mask)
 
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-
-    # 클라에서 데이터 받기
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print("closeing conn to ", data.addr)
-            sel.unregister(sock)
-            sock.close()
-
-    # 클라에 데이터 보내기
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print("echo ", repr(data.outb), "to", data.addr)
-            # sent = sock.send(data.outb)
-            # data.outb = data.outb[sent:]
-            sock.send(b"HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body>Hello World</body></html>\n")
-            print("closeing conn to ", data.addr)
-            sel.unregister(sock)
-            sock.close()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.sel.close()
 
 
-host, port = "127.0.0.1", 65432
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-print("listion on ", (host, port))
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
+    def accept_wrapper(self, sock):
+        conn, addr = sock.accept()
+        print("accepted connection from ", addr)
+        conn.setblocking(False)
+        data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        self.sel.register(conn, events, data=data)
 
-try:
-    while True:
-        events = sel.select(timeout=10)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
+    def service_connection(self, key, mask):
+        sock = key.fileobj
+        data = key.data
+
+        # 클라에서 데이터 받기
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)
+            if recv_data:
+                data.outb += recv_data
             else:
-                service_connection(key, mask)
+                print("closeing conn to ", data.addr)
+                self.sel.unregister(sock)
+                sock.close()
 
-except KeyboardInterrupt:
-    pass
-finally:
-    sel.close()
+        # 클라에 데이터 보내기
+        if mask & selectors.EVENT_WRITE:
+            if data.outb:
+                print("echo ", repr(data.outb), "to", data.addr)
+                # sent = sock.send(data.outb)
+                # data.outb = data.outb[sent:]
+                sock.send(b"HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body>Hello World</body></html>\n")
+                print("closeing conn to ", data.addr)
+                self.sel.unregister(sock)
+                sock.close()
 
+
+if __name__ == '__main__':
+    wfw = WebFrameWork()
+    wfw()
