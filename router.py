3715130e7d31249cbcdae7b8497b3802ce5c6a0b
uiandwe <uiandwe@gmail.com>
2019-12-27 00:36:32 +0900

    Update router
    - 파라미터 리턴 추가

--- router.py
+++ router.py
@@ -2,44 +2,51 @@
 # https://www.slideshare.net/kwatch/how-to-make-the-fastest-router-in-python
 
 import re
+import types
 
 
 def prefix_str(s):
     return s.split('{', 1)[0]
 
-
+# TODO func 빼기
+# TODO 변수 이름 바꾸기
+#
 class Router:
     def __init__(self, mapping):
         self.mapping_list = []
         self.mapping_dict = {}
-        for path, klass, funcs in mapping:
+        for path, funcs in mapping:
             if '{' not in path:
-                self.mapping_dict[path] = (klass, funcs, [])
+                self.mapping_dict[path] = (funcs, [])
                 continue
             prefix = prefix_str(path)
             # TODO 문자열 정규형 추가
             # int 정규형
             rexp_path = re.sub('\{\w*:int\}', '\d*', path)
             rexp = re.compile(rexp_path)
-            self.mapping_list.append((prefix, rexp, path, klass, funcs))
+            self.mapping_list.append((prefix, rexp, path, funcs))
 
     def lookup(self, req_method, req_path):
         t = self.mapping_dict.get(req_path)
         if t:
-            klass, funcs, parms = t
+            funcs, parms = t
             func = funcs.get(req_method)
-            return klass, func, []
+            return func, []
 
-        for prefix, rexp, path, klass, funcs in self.mapping_list:
+        for prefix, rexp, path, funcs in self.mapping_list:
             if not req_path.startswith(prefix):
                 continue
             m = rexp.match(req_path)
             if m:
-                print(m)
-                parms = [int(v) for v in m.groups()]
-                print(parms)
+                parms = []
+                for origin_data, req_data in zip(path.split("/"), req_path.split("/")):
+                    if origin_data == req_data:
+                        continue
+                    temp = re.search('\{(\w*):(\w*)\}', origin_data).groups()
+                    data = types.SimpleNamespace(name=temp[0], type=temp[1], data=req_data)
+                    parms.append(data)
                 func = funcs.get(req_method)
-                return klass, func, parms
+                return func, parms
 
         return None, None, None
 
@@ -52,15 +59,16 @@ if __name__ == '__main__':
     # path, class, {method: func,}
     mapping_list = [
 
-        (r"/api/books/", BooksAPI, {"GET": BooksAPI.do_index, "POST": BooksAPI.do_create}),
-        (r"/api/books/{id:int}/", BooksAPI, {"GET": BooksAPI.do_index, "PUT": BooksAPI.do_create}),
-        (r"/api/orders/", OrdersApi, {"GET": OrdersApi.do_show, "POST": OrdersApi.do_update})
+        (r"/api/books/", {"GET": BooksAPI.do_index, "POST": BooksAPI.do_create}),
+        (r"/api/books/{id:int}/", {"GET": BooksAPI.do_index, "PUT": BooksAPI.do_create}),
+        (r"/api/orders/", {"GET": OrdersApi.do_show, "POST": OrdersApi.do_update})
     ]
 
     router = Router(mapping_list)
-    assert router.lookup("GET", "/api/books/") == (BooksAPI, BooksAPI.do_index, [])
-    assert router.lookup("POST", "/api/books/") == (BooksAPI, BooksAPI.do_create, [])
-    print(router.lookup("PUT", "/api/books/123/"))
-    # assert router.lookup("GET", "/api/books/123/") == (BooksAPI, BooksAPI.do_index, [123])
-
+    assert router.lookup("GET", "/api/books/") == (BooksAPI.do_index, [])
+    assert router.lookup("POST", "/api/books/") == (BooksAPI.do_create, [])
+    assert router.lookup("GET", "/api/orders/") == (OrdersApi.do_show, [])
+    assert router.lookup("POST", "/api/orders/") == (OrdersApi.do_update, [])
+    assert router.lookup("GET", "/api/books/123/") == (BooksAPI.do_index, [types.SimpleNamespace(name='id', type='int', data='123')])
+    # TODO 예외처리 추가 하기
     # assert router.lookup("GET", "/api/books/23/test/") == (None, None, None)
